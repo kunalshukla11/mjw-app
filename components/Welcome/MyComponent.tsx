ecfcdaf94f67c1533c22d4080c7d193decc4a417
Narendra <narendra1192cs@gmail.com>
2024-02-10 09:48:35 +0000

    Adding primary Colour

--- components/Welcome/MyComponent.tsx
+++ components/Welcome/MyComponent.tsx
@@ -1,4 +1,4 @@
-import { Button, Group } from '@mantine/core';
+import { Button, Group, Input } from '@mantine/core';
 import mycomponent from './MyComponent.module.css';
 
 export function MyComponent() {
@@ -8,6 +8,7 @@ export function MyComponent() {
         <h1 className={mycomponent.heading}>MyComponent</h1>
         <h2>Start editing to see some magic happen!</h2>
         <Button className="bg-red-400">Button</Button>
+        <Input placeholder="Heys"></Input>
       </Group>
     </>
   );
--- theme.ts
+++ theme.ts
@@ -3,5 +3,6 @@
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
+  primaryColor: 'indigo',
   /* Put your mantine theme override here */
 });
