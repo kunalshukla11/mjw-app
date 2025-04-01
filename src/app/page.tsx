c1bbab5e1b32551a5df99ed713e8b8a7dfa367c5
Narendra <narendra1192cs@gmail.com>
2025-04-01 20:29:08 +0100

    comment dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
 
 import MainLayout from '@/src/components/layout/MainLayout';
 import { getHolidayDashboard } from '../lib/services/holiday/holidayService';
