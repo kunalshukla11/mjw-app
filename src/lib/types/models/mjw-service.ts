6647ee39b47a3f62721675edc615509294b0e8d5
Narendra <narendra1192cs@gmail.com>
2025-11-27 15:02:59 +0530

    adding cross env

--- src/lib/types/models/mjw-service.ts
+++ src/lib/types/models/mjw-service.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2025-11-07 13:55:57.
+// Generated using typescript-generator version 3.2.1263 on 2025-11-27 14:53:25.
 
 export interface Review {
     comment: string;
